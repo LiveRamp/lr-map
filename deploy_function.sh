12d433182be9f15d638537be77cf2f62cb6d5bdb
Tomasz Flendrich <tomasz.flendrich@liveramp.com>
2017-07-11 20:51:16 -0700

    cleanup bash script

--- deploy_function.sh
+++ deploy_function.sh
@@ -1,2 +1,7 @@
-ditto -c -k --sequesterRsrc lambda_package archive.zip
-aws lambda update-function-code --function-name functionv5 --zip-file fileb://archive.zip
+ARCHIVE_FILENAME="archive_temporary123455125214.zip" # guaranteed to be unique
+AWS_LAMBDA_FUNCTION_NAME="functionv5"
+DIRECTORY_WITH_CONTENT="./lambda_package"
+
+ditto -c -k --sequesterRsrc $DIRECTORY_WITH_CONTENT $ARCHIVE_FILENAME
+aws lambda update-function-code --function-name $AWS_LAMBDA_FUNCTION_NAME --zip-file fileb://$ARCHIVE_FILENAME
+rm $ARCHIVE_FILENAME
