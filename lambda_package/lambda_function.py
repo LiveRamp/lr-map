74f2f58bc6c20237030fe24e610f4db33f4a6bfe
Florian Gauger <flo.gauger@gmail.com>
2017-07-11 17:15:38 -0700

    Split request processing

--- lambda_package/lambda_function.py
+++ lambda_package/lambda_function.py
@@ -1,42 +1,3 @@
-'''
-This function handles a Slack slash command and echoes the details back to the user.
-
-Follow these steps to configure the slash command in Slack:
-
-  1. Navigate to https://<your-team-domain>.slack.com/services/new
-
-  2. Search for and select "Slash Commands".
-
-  3. Enter a name for your command and click "Add Slash Command Integration".
-
-  4. Copy the token string from the integration settings and use it in the next section.
-
-  5. After you complete this blueprint, enter the provided API endpoint URL in the URL field.
-
-
-To encrypt your secrets use the following steps:
-
-  1. Create or use an existing KMS Key - http://docs.aws.amazon.com/kms/latest/developerguide/create-keys.html
-
-  2. Click the "Enable Encryption Helpers" checkbox
-
-  3. Paste <COMMAND_TOKEN> into the kmsEncryptedToken environment variable and click encrypt
-
-
-Follow these steps to complete the configuration of your command API endpoint
-
-  1. When completing the blueprint configuration select "Open" for security
-     on the "Configure triggers" page.
-
-  2. Enter a name for your execution role in the "Role name" field.
-     Your function's execution role needs kms:Decrypt permissions. We have
-     pre-selected the "KMS decryption permissions" policy template that will
-     automatically add these permissions.
-
-  3. Update the URL for your Slack slash command with the invocation URL for the
-     created API resource in the prod stage.
-'''
-
 import boto3
 import json
 import logging
@@ -57,31 +18,40 @@ logger.setLevel(logging.INFO)
 
 def respond(err, res=None):
     return {
-        'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
-        'headers': {
-            'Content-Type': 'application/json',
-        },
-    }
+            'statusCode': '400' if err else '200',
+            'body': err.message if err else json.dumps(res),
+            'headers': {
+                'Content-Type': 'application/json',
+                },
+            }
 
-
-def lambda_handler(event, context):
+def create_and_upload_image(event, context):
     # arguments = event[u'queryStringParameters'][u'text']
-
     # argumentsInt = int(arguments)
-
+    location_x = 0.5
+    location_y = 0.5
 
     s3 = boto3.resource('s3')
     s3_client = boto3.client('s3')
 
     bucket = "maps42"
-    filename = "newfile" + str(time.strftime("%H:%M:%S")) + ".txt"
+    filename = "newfile" + str(time.strftime("%H:%M:%S")) + ".gif"
     filepath = "/tmp/" + filename
-    file = open(filepath, 'w+')
-    file.write("abc")
-    s3_client.upload_file('/tmp/' + filename, bucket, filename)
+    create_location_image(location_x, location_y, filepath)
+    s3_client.upload_file(filepath, bucket, filename)
 
-    image_url =  "https://s3.amazonaws.com/maps42/result.gif"
+    image_url =  "https://s3.amazonaws.com/maps42/" + filepath
 
     response = create_slack_response("Tomasz", image_url)
     return respond(None, response)
+
+
+def lambda_handler(event, context):
+
+    request_type = "create_and_upload_image" #<todo tomasz>
+
+    request_type_to_action = {
+            "create_and_upload_image": create_and_upload_image
+            }
+
+    return request_type_to_action[request_type](event, context)
