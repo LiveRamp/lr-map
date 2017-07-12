9137af75c8ca1b56900cc0447a3a4465222b82a2
Tomasz Flendrich <tomasz.flendrich@liveramp.com>
2017-07-11 21:26:42 -0700

    big image is working

--- lambda_package/image.py
+++ lambda_package/image.py
@@ -3,7 +3,7 @@ import operator, math
 
 def create_location_image (drop_location_x, drop_location_y, result_path):
 
-    floor_map = Image.open( './16th.png' )
+    floor_map = Image.open( './16th_big.png' )
     floor_map_copy = floor_map.copy()
     drop_location = (int(drop_location_x * floor_map.width), int(drop_location_y * floor_map.height))
 
