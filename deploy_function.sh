034ce63fdcd7bd5cf8d109fc8289c3d7be59def7
Tomasz Flendrich <tomasz.flendrich@liveramp.com>
2017-07-12 12:12:19 -0700

    change project structure

--- deploy_function.sh
+++ deploy_function.sh
@@ -1,7 +1,14 @@
 ARCHIVE_FILENAME="archive_temporary123455125214.zip" # guaranteed to be unique
 AWS_LAMBDA_FUNCTION_NAME="functionv5"
-DIRECTORY_WITH_CONTENT="./lambda_package"
+DIRECTORY_WITH_CONTENT="./code" 
+DIRECTORY_WITH_FILES_TO_BE_COPIED="./environment"
+BUILD_DIR="./build"
 
-ditto -c -k --sequesterRsrc $DIRECTORY_WITH_CONTENT $ARCHIVE_FILENAME
+mkdir $BUILD_DIR
+cp -r $DIRECTORY_WITH_CONTENT/* $BUILD_DIR
+cp -r $DIRECTORY_WITH_FILES_TO_BE_COPIED/* $BUILD_DIR
+
+
+ditto -c -k --sequesterRsrc $BUILD_DIR $ARCHIVE_FILENAME
 aws lambda update-function-code --function-name $AWS_LAMBDA_FUNCTION_NAME --zip-file fileb://$ARCHIVE_FILENAME
 rm $ARCHIVE_FILENAME
