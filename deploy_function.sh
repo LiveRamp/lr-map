195297051fd54e7dd091c2c10244e15ec7423e36
Tomasz Flendrich <tomasz.flendrich@liveramp.com>
2017-07-14 14:40:06 -0700

    start sending to test too

--- deploy_function.sh
+++ deploy_function.sh
@@ -14,21 +14,26 @@ mkdir $BUILD_DIR
 cp -r $DIRECTORY_WITH_CONTENT/* $BUILD_DIR
 cp -r $DIRECTORY_WITH_FILES_TO_BE_COPIED/* $BUILD_DIR
 
+AWS_PROFILE=${AWS_PROFILE:-"TEST"}
+CLI_PROFILE_ARG=" --profile $AWS_PROFILE"
 
+if [ "$AWS_PROFILE" = "TEST" ]; then
+    S3_STATIC_CONTENT_BUCKET_NAME="$S3_STATIC_CONTENT_BUCKET_NAME-test" # nothing if on production
+fi
 
 ditto -c -k --sequesterRsrc $BUILD_DIR $MAIN_ARCHIVE_FILENAME
 ditto -c -k --sequesterRsrc $DIRECTORY_WITH_DB_HELPER $DB_HELPER_ARCHIVE_FILENAME
 
-aws lambda update-function-code --function-name $AWS_LAMBDA_MAIN_FUNCTION_NAME --zip-file fileb://$MAIN_ARCHIVE_FILENAME
-aws lambda update-function-code --function-name $AWS_LAMBDA_ADD_TO_DB_FUNCTION_NAME --zip-file fileb://$DB_HELPER_ARCHIVE_FILENAME
+aws lambda update-function-code --function-name $AWS_LAMBDA_MAIN_FUNCTION_NAME --zip-file fileb://$MAIN_ARCHIVE_FILENAME $CLI_PROFILE_ARG
+aws lambda update-function-code --function-name $AWS_LAMBDA_ADD_TO_DB_FUNCTION_NAME --zip-file fileb://$DB_HELPER_ARCHIVE_FILENAME $CLI_PROFILE_ARG
 
 
-aws s3 cp ./code/frontend/index.html s3://$S3_STATIC_CONTENT_BUCKET_NAME/index.html
-aws s3 cp ./code/frontend/index.js s3://$S3_STATIC_CONTENT_BUCKET_NAME/index.js
-aws s3 cp ./code/frontend/style.css s3://$S3_STATIC_CONTENT_BUCKET_NAME/style.css
-aws s3 cp ./code/frontend/img/16th.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/16th.png
-aws s3 cp ./code/frontend/img/17th.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/17th.png
-aws s3 cp ./code/frontend/img/pin.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/pin.png
+aws s3 cp ./code/frontend/index.html s3://$S3_STATIC_CONTENT_BUCKET_NAME$/index.html $CLI_PROFILE_ARG
+aws s3 cp ./code/frontend/index.js s3://$S3_STATIC_CONTENT_BUCKET_NAME/index.js $CLI_PROFILE_ARG
+aws s3 cp ./code/frontend/style.css s3://$S3_STATIC_CONTENT_BUCKET_NAME/style.css $CLI_PROFILE_ARG
+aws s3 cp ./code/frontend/img/16th.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/16th.png $CLI_PROFILE_ARG
+aws s3 cp ./code/frontend/img/17th.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/17th.png $CLI_PROFILE_ARG
+aws s3 cp ./code/frontend/img/pin.png s3://$S3_STATIC_CONTENT_BUCKET_NAME/img/pin.png $CLI_PROFILE_ARG
 
 rm -r $BUILD_DIR
 # rm $MAIN_ARCHIVE_FILENAME
