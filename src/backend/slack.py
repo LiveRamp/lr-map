d7268f00d5deee1a9bd0ff2dc406d22c794c3041
Florian Gauger <flo.gauger@gmail.com>
2017-07-26 16:38:39 -0700

    Add close button to guide command

--- src/backend/slack.py
+++ src/backend/slack.py
@@ -19,45 +19,39 @@ def create_send_slack_message(in_channel, location_for, change_url, image_url, c
 
 
 def create_slack_response (in_channel, location_for, image_url, change_url, created_by, created_on, token):
-    return json.dumps(
-            {
-                "attachments": [
-                    {
-                        "title": "Location of " + location_for + " [/map " + location_for + "]",
-                        "text": "Click <" + change_url + "|here> to update.",
-                        "color": "#36a64f",
-                        "image_url": image_url,
-                        "fallback": "Someone sent you a location via /map.",
-                        "callback_id": "map",
-                        "attachment_type": "default",
-			"footer": "Location added by " + created_by + " <!date^" + created_on + "^ on {date} at {time}.|.>",
-                        "actions": [
-                            {
-                                "name": "send8037123",
-                                "text": "Send",
-                                "type": "button",
-                                "value": create_send_slack_message(in_channel, location_for, change_url, image_url, created_by, created_on, token),
-                                "style": "primary"
-                                },
-                            {
-                                "name": "cancel8037123",
-                                "text": "Cancel",
-                                "type": "button",
-                                "value": "cancel"
-                                }
-                            ]
+    return json.dumps({
+                "attachments": [{
+                    "title": "Location of " + location_for + " [/map " + location_for + "]",
+                    "text": "Click <" + change_url + "|here> to update.",
+                    "color": "#36a64f",
+                    "image_url": image_url,
+                    "fallback": "Someone sent you a location via /map.",
+                    "callback_id": "map",
+                    "attachment_type": "default",
+                    "footer": "Location added by " + created_by + " <!date^" + created_on + "^ on {date} at {time}.|.>",
+                    "actions": [
+                        {
+                            "name": "send8037123",
+                            "text": "Send",
+                            "type": "button",
+                            "value": create_send_slack_message(in_channel, location_for, change_url, image_url, created_by, created_on, token),
+                            "style": "primary"
+                        },
+                        {
+                            "name": "cancel8037123",
+                            "text": "Cancel",
+                            "type": "button",
+                            "value": "cancel"
                         }
                     ]
-                }
-            )
+                }]
+            })
 
 def create_slack_response_not_found (location_for, change_url):
-    return json.dumps(
-            {
+    return json.dumps({
                 "text": "Whoops, it looks like we don't know where *" + location_for + "* is :sadparrot:.\n" 
                 + "Please help us out by setting the location *<" + change_url + "|here>*!"
-            }
-        )
+            })
 
 def create_slack_auth_response (client_id, redirect_uri, team):
     url = "https://slack.com/oauth/authorize?"
@@ -67,43 +61,44 @@ def create_slack_auth_response (client_id, redirect_uri, team):
         "redirect_uri": redirect_uri,
         "team": team,
         })
-    return json.dumps(
-            {
+    return json.dumps({
                 "text": ":authoritah: Please authorize this app *<" + url + "|here>*!"
-            }
-        )
+            })
 
 def create_failed_slack_response (message):
-    return json.dumps(
-            {
+    return json.dumps({
                 "text": "Something went terribly wrong :explodyparrot:. Please contact @tflend.\n`" + message + "`"
-            }
-        )
+            })
 
 def create_slack_guide (created_by):
-    return json.dumps(
-        {
+    return json.dumps({
             "text": "Hi " + created_by+ " please take a look below to see how */map* works.\nFor a complete guide please click *<https://support.liveramp.com/display/CI/Find+and+share+meeting+rooms%2C+people%27s+desks+and+other+locations|here>*.",
-            "attachments": [
-                {
-                    "fallback": "/map Quick Guide",
-                    "color": "#36a64f",
-                    "title": "Quick Guide",
-                    "fields": [
-                        {
-                            "title": "/map apollo",
-                            "value": "Search for the location of any meeting room.",
-                        },
-                        {
-                            "title": "/map @userNameTag",
-                            "value": "Search for the location of a persons desk.\nHint: Please set your own desk location by typing: /map " + created_by,
-                        },
-                        {
-                            "title": "/map yourLocation",
-                            "value": "Use any other string to temporarly set the location of anything.",
-                        }
-                    ],
-                }
-            ]
-        }
-    )
+            "attachments": [{
+                "fallback": "/map Quick Guide",
+                "color": "#36a64f",
+                "title": "Quick Guide",
+                "callback_id": "map",
+                "fields": [
+                    {
+                        "title": "/map apollo",
+                        "value": "Search for the location of any meeting room.",
+                    },
+                    {
+                        "title": "/map @userNameTag",
+                        "value": "Search for the location of a persons desk.\nHint: Please set your own desk location by typing: /map " + created_by,
+                    },
+                    {
+                        "title": "/map yourLocation",
+                        "value": "Use any other string to temporarly set the location of anything.",
+                    }
+                ],
+                "actions": [
+                    {
+                        "name": "cancel8037123",
+                        "text": "Close",
+                        "type": "button",
+                        "value": "cancel"
+                    }
+                ]
+            }]
+        })
