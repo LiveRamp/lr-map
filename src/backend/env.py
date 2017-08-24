22c98950849993cb6287b93c90f50d52fe293d1c
Tomasz Flendrich <tomasz.flendrich@liveramp.com>
2017-08-23 19:15:33 -0700

    remove secrets

--- src/backend/env.py
+++ src/backend/env.py
@@ -2,23 +2,22 @@ import os
 import sys
 
 if "PROD" in os.environ:
-    link_to_frontend = "http://***REMOVED***.s3-website-us-east-1.amazonaws.com/"
-    link_to_db_helper = 'https://***REMOVED***.execute-api.us-east-1.amazonaws.com/prod/***REMOVED***-db-helper'
-    url = "https://slack.com/oauth/authorize?client_id=***REMOVED***&scope=chat%3Awrite%3Auser&redirect_uri=https%3A%2F%2F***REMOVED***.execute-api.us-east-1.amazonaws.com%2Fprod%2F***REMOVED***-db-helper-staging"
-    bucket = "slack-map-images"
-    LOCATIONS_TABLE_NAME = "MapLocations"
-    AUTH_TABLE_NAME = "AuthTokens"
-    slack_client_id = "***REMOVED***"
-    slack_client_secret = "***REMOVED***"
-    team_id = "***REMOVED***"
+    link_to_frontend =
+    link_to_db_helper =
+    bucket =
+    LOCATIONS_TABLE_NAME =
+    AUTH_TABLE_NAME =
+    slack_client_id =
+    slack_client_secret =
+    team_id =
 elif "TEST" in os.environ:
-    link_to_frontend = "http://***REMOVED***-staging.s3-website-us-east-1.amazonaws.com/"
-    link_to_db_helper = 'https://***REMOVED***.execute-api.us-east-1.amazonaws.com/prod/***REMOVED***-db-helper-staging'
-    bucket = "***REMOVED***"
-    LOCATIONS_TABLE_NAME = "MapLocationsStaging"
-    AUTH_TABLE_NAME = "AuthTokensStaging"
-    slack_client_id = "***REMOVED***"
-    slack_client_secret = "***REMOVED***"
-    team_id = "***REMOVED***"
+    link_to_frontend =
+    link_to_db_helper =
+    bucket =
+    LOCATIONS_TABLE_NAME =
+    AUTH_TABLE_NAME =
+    slack_client_id =
+    slack_client_secret =
+    team_id =
 else:
     sys.exit(1)
